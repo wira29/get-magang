bdd0a20a234da04f204b4fb6df11dcefe89d99e9
Wira29 <bag4sk4r4@gmail.com>
2022-11-01 17:46:53 +0700

    first commit

---
+++ database/migrations/2022_11_01_102335_create_schools_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void;
+     */
+    public function up()
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('school_name', 150);
+            $table->string('email')->unique();
+            $table->string('contact');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools');
+    }
+};
---
+++ database/migrations/2022_11_01_102518_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('student_name', 150);
+            $table->enum('gender', ['male', 'female']);
+            $table->foreignUuid('school_id')->constrained('schools');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
